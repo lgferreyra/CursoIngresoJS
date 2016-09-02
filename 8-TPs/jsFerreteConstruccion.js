f3c9151947e7579ad391fed263d1cdcb729f7fe4
lgferreyra <leonardogferreyra@gmail.com>
2016-09-02 00:46:27 -0300

    blabla

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,16 +3,25 @@ A.	 Mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	Mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
+var app = angular.module('myApp', []);
 
-function Rectangulo () 
-{
 
-}
-function Circulo () 
-{
-	
-}
-function Materiales () 
-{
-	
-}
+app.controller('myController', function($scope) {
+  
+	$scope.Rectangulo = function() 
+	{
+		var total = (parseInt($scope.largo)*2) + (parseInt($scope.ancho)*2);
+		alert(total*3);
+	}
+	$scope.Circulo = function() 
+	{
+		var total = (parseInt($scope.radio)*2)*3.14;
+		alert(total*3);
+	}
+	$scope.Materiales = function() 
+	{
+		var totalMetrosCuadrados = parseInt($scope.largo)*parseInt($scope.ancho);
+		alert("Cemento: " + (totalMetrosCuadrados*2) + ". Cal: " + (totalMetrosCuadrados*3) + ".");
+	}
+
+});
--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,20 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
-{
-	
-}
+var app = angular.module('myApp', []);
 
-function CentigradosFahrenheit () 
-{
-	
-}
+
+app.controller('myController', function($scope) {
+  
+	$scope.FahrenheitCentigrados = function() 
+	{
+		var celcius = parseInt($scope.temperatura) - 32;
+		alert($scope.temperatura + " Fahrenheit son " + celcius + " centígrados");
+	}
+	$scope.CentigradosFahrenheit = function() 
+	{
+		var fahrenheit = parseInt($scope.temperatura) + 32;
+		alert($scope.temperatura + " centígrados son " + fahrenheit + " Fahrenheit");
+	}
+
+});
